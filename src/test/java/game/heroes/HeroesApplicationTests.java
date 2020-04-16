5cb2d8eb96119338b9e07c798098e4ac215cd20f
vladitodorov <vladitodorov81@gmail.com>
2020-04-16 17:14:20 +0300

    Initial commit for project

---
+++ src/test/java/game/heroes/HeroesApplicationTests.java
@@ -0,0 +1,13 @@
+package game.heroes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
